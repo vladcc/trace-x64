8a1c2f88b429874889b238a0a030633c20e4c811
Vladimir Dinev <vld.dinev@gmail.com>
2023-07-20 22:57:42 +0200

    make sure added; run-tests.sh &&-ed together

--- trace-x64/tests/run-tests.sh
+++ trace-x64/tests/run-tests.sh
@@ -3,10 +3,10 @@
 function main
 {
 	local MY_DIR="$(dirname $(realpath $0))"
-	"${MY_DIR}/cleanup.sh" "${MY_DIR}"
-	"${MY_DIR}/run-tests-tx64-trace.sh" "$@"
-	"${MY_DIR}/run-tests-tx64-print.sh" "$@"
-	"${MY_DIR}/run-tests-sym-map.sh" "$@"
+	"${MY_DIR}/cleanup.sh" "${MY_DIR}"       && \
+	"${MY_DIR}/run-tests-tx64-trace.sh" "$@" && \
+	"${MY_DIR}/run-tests-tx64-print.sh" "$@" && \
+	"${MY_DIR}/run-tests-sym-map.sh" "$@"    && \
 	"${MY_DIR}/run-tests-pipeline.sh" "$@"
 }
 
